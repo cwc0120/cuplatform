fd999f8aac067f78e76a019903e6aa8c0ca8c625
cwc0120 <waiwai19930120@gmail.com>
2016-04-13 17:27:16 +0800

    Profile UI fix

--- public/javascripts/controllers/profilecontroller.js
+++ public/javascripts/controllers/profilecontroller.js
@@ -60,7 +60,7 @@ ctrl.controller('profileController', function($scope, $window, $location, $route
 			clickOutsideToClose: true
 		}).then(function(fd) {
 			User.uploadIcon(uid, fd).success(function(res) {
-				$mdToast.show($mdToast.simple().textContent('Password is changed. Please log in again.'));
+				$mdToast.show($mdToast.simple().textContent('Your avatar is uploaded successfully.'));
 				$scope.user = res;
 			}).error(function(res) {
 				$scope.success = false;
