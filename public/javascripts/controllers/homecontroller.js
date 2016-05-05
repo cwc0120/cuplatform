f0149ab3b7c8d53d65ec76f1ff989486daa0f254
cwc0120 <waiwai19930120@gmail.com>
2016-05-06 00:00:38 +0800

    commented

--- public/javascripts/controllers/homecontroller.js
+++ public/javascripts/controllers/homecontroller.js
@@ -1,9 +1,24 @@
 'use strict';
+
+// Module: Home
+// Purpose: 
+// 	This module is used to provide login/register access on the homepage and
+// 	show basic information if the user is logged in.
+// Interface:
+// 	$scope.login: user login (visitor is required)
+// 	$scope.registerDialog: visitor register (visitor is required)
+// 	$scope.deleteUpdate: delete an update in update center (login is required)
+
 ctrl.controller('homeController', function($scope, $location, $mdDialog, $mdToast, Auth, Socket, User) {
+	// Variables
 	$scope.user = {};
 	$scope.disable = true;
 	$scope.$location = $location;
 
+	// Initialization: 
+	// if the user is logged in, send a request to server to
+	// retrieve the timetable of the user. Then, decode the server data and 
+	// display on the webpage
 	if (Auth.isLogged) {
 		User.getTimetable(Auth.uid).success(function(res) {
 			$scope.success = true;
@@ -39,6 +54,10 @@ ctrl.controller('homeController', function($scope, $location, $mdDialog, $mdToas
 		});
 	}
 
+	// check the state of the user (logged-in or logged-out) to determine the
+	// content on the website.
+	// if the user is logged-in, a timetable and update center should be shown
+	// otherwise, introduction of CUP and login box is shown
 	$scope.$watch(function() {
 		return Auth.isLogged;
 	}, function(newVal, oldVal) {
@@ -47,6 +66,10 @@ ctrl.controller('homeController', function($scope, $location, $mdDialog, $mdToas
 		}
 	});
 
+	// check the state of the user (logged-in or logged-out) to determine the
+	// content on the website.
+	// if the user is logged-in, a request is sent to server to retrieve
+	// update messages of the user
 	$scope.$watch(function() {
 		return Auth.uid;
 	}, function(newVal, oldVal) {
@@ -58,6 +81,16 @@ ctrl.controller('homeController', function($scope, $location, $mdDialog, $mdToas
 		}
 	});
 
+	//--------------------------------------------------------------------------
+	// Name: $scope.login
+	// Purpose: user login (visitor is required)
+	// Input: user id, password
+	// Output: token, user id, admin
+	// Implementation:
+	// 	Send a login request to the server. If successful, set localStorage of
+	// 	of the browser to store the token and other user information. Also,
+	// 	connect the user to socket for messenger module. Finally, retrieve the 
+	// 	timetable of the user from server and display it on the webpage.
 	$scope.login = function() {
 		if ($scope.user.uid !== undefined && $scope.user.pwd !== undefined) {
 			Auth.login($scope.user).success(function(res) {
@@ -106,6 +139,15 @@ ctrl.controller('homeController', function($scope, $location, $mdDialog, $mdToas
 		}
 	};
 
+	//--------------------------------------------------------------------------
+	// Name: $scope.registerDialog
+	// Purpose: ask visitor to register
+	// Input: user id, email, password, password confirmation, icon
+	// Output: a token if registration is successful
+	// Implementation:
+	// 	First show a dialog with register.html for visitor's input. Once 
+	// 	received input, send the new user information to the server. The server 
+	// 	add the new user and re-route the user to homepage.
 	$scope.registerDialog = function(event) {
 		$mdDialog.show({
 			controller: registerController,
@@ -128,13 +170,16 @@ ctrl.controller('homeController', function($scope, $location, $mdDialog, $mdToas
 		});
 	};
 
+	// controller of register dialog
 	function registerController($scope, $mdDialog, Auth) {
 		$scope.newUser = {};
 
+		// quit this dialog
 		$scope.cancel = function() {
 			$mdDialog.cancel();
 		};
 
+		// send the user information to the server for registration
 		$scope.createUser = function() {
 			var fd = new FormData();
 			fd.append('img', $scope.img);
@@ -150,6 +195,15 @@ ctrl.controller('homeController', function($scope, $location, $mdDialog, $mdToas
 		};
 	}
 
+	//--------------------------------------------------------------------------
+	// Name: $scope.deleteUpdate
+	// Purpose: delete an update in update center (login is required)
+	// Input: update id
+	// Output: a updated update message list
+	// Implementation:
+	// 	Send a delete request to the server. The server will delete the update 
+	// 	message and send back the updated update message list which is displayed 
+	// 	on the webpage.
 	$scope.deleteUpdate = function(updateID) {
 		User.deleteUpdate(updateID).success(function(res) {
 			$scope.updates = res.updates;
