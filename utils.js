2243665a9fb0d857f5884c6c20313f85272d415b
cwc0120 <waiwai19930120@gmail.com>
2016-05-06 17:48:59 +0800

    commented

--- utils.js
+++ utils.js
@@ -1,6 +1,28 @@
 'use strict';
 
+// Module: Utils
+// Purpose: 
+// 	This module contains methods which are commonly used.
+// Interface:
+// 	validateToken: check if the request contains a token. If no token, force log out
+// 	validateTokenPartial: check if the request contains a token, but don't force log out
+// 		if no token
+// 	findUser: decode token (for socket's use)
+// 	addPoint: add points to a user
+// 	deductPoint: deduct points from a user
+// 	informUser: send an update message to a user
+// 	informAdmin: send an update message to all admin
+
 module.exports = {
+	//--------------------------------------------------------------------------
+	// Name: validateToken
+	// Purpose: check if the request contains a token. If no token, force log out
+	// Input: token
+	// Output: a decoded token
+	// Implementation:
+	// 	check if the request contains a token
+	// 	if true, try to decode it
+	// 	otherwise, send an 401 error which forces user to log out
 	validateToken: function(req, res, next) {
 		var jwt = require('jsonwebtoken');
 		var token = req.headers['x-access-token'];
@@ -19,6 +41,16 @@ module.exports = {
 		}
 	},
 
+	//--------------------------------------------------------------------------
+	// Name: validateTokenPartial
+	// Purpose: check if the request contains a token, but don't force log out
+	// 	if no token
+	// Input: token
+	// Output: a decoded token
+	// Implementation:
+	// 	check if the request contains a token
+	// 	if true, try to decode it and execute next middleware
+	// 	otherwise, ignore it and execute next middleware
 	validateTokenPartial: function(req, res, next) {
 		var jwt = require('jsonwebtoken');
 		var token = req.headers['x-access-token'];
@@ -37,6 +69,14 @@ module.exports = {
 		}
 	},
 
+	//--------------------------------------------------------------------------
+	// Name: findUser
+	// Purpose: decode token (for socket's use)
+	// Input: token, callback
+	// Output: a decoded token
+	// Implementation:
+	// 	check if the request contains a token
+	// 	if true, try to decode it and execute next callback
 	findUser: function(token, callback) {
 		var config = require('./config');
 		var jwt = require('jsonwebtoken');
@@ -52,6 +92,14 @@ module.exports = {
 		}
 	},
 
+	//--------------------------------------------------------------------------
+	// Name: addPoint
+	// Purpose: add points to a user
+	// Input: user id, point, callback
+	// Output: none
+	// Implementation:
+	// 	find the user in database. If successful, add points to the user and
+	// 	execute next callback
 	addPoint: function(uid, point, callback) {
 		var User = require('./models/user');
 
@@ -64,6 +112,14 @@ module.exports = {
 		});
 	},
 
+	//--------------------------------------------------------------------------
+	// Name: deductPoint
+	// Purpose: deduct points to a user
+	// Input: user id, point, callback
+	// Output: none
+	// Implementation:
+	// 	find the user in database. If successful, deduct points to the user and
+	// 	execute next callback
 	deductPoint: function(uid, point, callback) {
 		var User = require('./models/user');
 
@@ -76,6 +132,14 @@ module.exports = {
 		});
 	},
 
+	//--------------------------------------------------------------------------
+	// Name: informUser
+	// Purpose: send an update message to a user
+	// Input: user id, message, callback
+	// Output: none
+	// Implementation:
+	// 	find the user in database. If successful, push the update message to the
+	// 	user and execute next callback
 	informUser: function(uid, message, callback) {
 		var User = require('./models/user');
 
@@ -88,6 +152,14 @@ module.exports = {
 		});
 	},
 
+	//--------------------------------------------------------------------------
+	// Name: informAdmin
+	// Purpose: send an update message to all admin
+	// Input: message, callback
+	// Output: none
+	// Implementation:
+	// 	find all admin in database. If successful, push the update message to all
+	// 	the admin and execute next callback
 	informAdmin: function(message, callback) {
 		var User = require('./models/user');
 
