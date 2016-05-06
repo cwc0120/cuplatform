5be3c3c0a1b6ededaf9ad32f8d901f888f3b6906
cwc0120 <waiwai19930120@gmail.com>
2016-05-06 15:19:45 +0800

    commented

--- public/javascripts/controllers/tradingrecordcontroller.js
+++ public/javascripts/controllers/tradingrecordcontroller.js
@@ -1,17 +1,27 @@
 'use strict';
+
+// Module: Tradingrecord
+// Purpose: 
+// 	This module is used to show the trading history of the user
+// Interface:
+// 	$scope.back: return to previous page if error occurs
+
 ctrl.controller('tradingRecordController', function($scope, $window, $location, User) {
+	// Variables
 	$scope.$location = $location;
 	if ($window.localStorage['admin'] === 'true') {
 		$scope.admin = true;
 	} else {
 		$scope.admin = false;
 	}
-
 	$scope.orderBuy = 'name';
 	$scope.orderSell = 'name';
 	$scope.limit = 10;
 	$scope.page = 1;
 
+	// Initialization:
+	// send a request to server to retrieve items that was put on trading platform
+	// if successful, display item list on the webpage
 	User.getSellList().success(function(res) {
 		$scope.success = true;
 		$scope.selllist = res;
@@ -20,6 +30,9 @@ ctrl.controller('tradingRecordController', function($scope, $window, $location, 
 		$scope.errorMessage = res.error;
 	});
 
+	// Initialization:
+	// send a request to server to retrieve items that was interested/bought
+	// if successful, display item list on the webpage
 	User.getBuyList().success(function(res) {
 		$scope.success = true;
 		$scope.buylist = res;
@@ -28,6 +41,9 @@ ctrl.controller('tradingRecordController', function($scope, $window, $location, 
 		$scope.errorMessage = res.error;
 	});
 
+	//--------------------------------------------------------------------------
+	// Name: $scope.back
+	// Purpose: return to previous page if error occurs
 	$scope.back = function() {
 		window.history.back();
 	};
